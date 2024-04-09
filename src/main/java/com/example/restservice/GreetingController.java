decb8c650af49c9dc7635cb0914d1732182f4ac6
Ricardo Salles <salles.ricardo@gmail.com.br>
2024-04-09 19:49:40 -0300

    Fix typo

--- src/main/java/com/example/restservice/GreetingController.java
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
-    private final AtomicLong counter = newAtomicLong();
+    private final AtomicLong counter = new AtomicLong();
 
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
